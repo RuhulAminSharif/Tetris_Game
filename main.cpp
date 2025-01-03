d3a7f3f2d607fb9eb85d1b5b9b8eb3de75c510d0
Sharif <sharif.cse.ras@gmail.com>
2025-01-03 19:30:15 +0600

    completed grid set up

--- main.cpp
+++ main.cpp
@@ -4,34 +4,66 @@ using namespace std;
 #include <GL/glut.h>
 #define ll long long int
 #define ld long double
-ll x[2],y[2];
+/// Grid Setup
+class Grid
+{
+private:
+    ll numRows, numCols, cellSize;
+public:
+    vector<vector<int>> grid;
+    Grid()
+    {
+        numRows = 20;
+        numCols = 10;
+        cellSize = 30;
+        initialize();
+    }
+    void initialize() {
+        grid.resize(numRows);
+        for( ll row = 0; row < numRows; row += 1 ) {
+            grid[row].resize(numCols,0);
+        }
+    }
+    void print() {
+        for( ll row = 0; row < numRows; row += 1 ) {
+            for( ll col = 0; col < numCols; col += 1 ) {
+                cout << grid[row][col] << " ";
+            }
+            cout << endl;
+        }
+    }
+};
+
 void display(void)
 {
-    glClear(GL_COLOR_BUFFER_BIT);
-    glColor3f(1.0,1.0,1.0);
-    glPointSize(2.0);
+    /// Setting up Background color
+    glClear(GL_COLOR_BUFFER_BIT );
+    glClearColor(0.172, 0.172, 0.498, 1.0);
+    glutSwapBuffers();
+    Grid grid = Grid();
+    grid.print();
 
-    glFlush();
     return ;
 }
+
 void myInit (void)
 {
-	glClear(GL_COLOR_BUFFER_BIT);
-	glClearColor(0, 0, 0, 0);
+    glClear(GL_COLOR_BUFFER_BIT);
+	glClearColor(0.172f, 0.172f, 0.498f, 1.0f);
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-	gluOrtho2D(-500, 500, 500, -500);
+	gluOrtho2D(0.0, 1.0, 0.0, 1.0);
 	return ;
 }
 int main(int argc, char ** argv)
 {
 	glutInit( & argc, argv);
-	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-	glutInitWindowSize(100, 100);
+	glutInitDisplayMode(GLUT_DOUBLE| GLUT_RGB);
+	glutInitWindowSize(800, 600);
 	glutInitWindowPosition(100, 100);
 	glutCreateWindow("Tetris Game by Ruhul Amin Sharif");
 	myInit ();
 	glutDisplayFunc(display);
-	glutMainLoop();
+    glutMainLoop();
     return 0;
 }
